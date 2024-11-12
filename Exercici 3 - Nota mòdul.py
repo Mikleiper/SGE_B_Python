64a8b3ce1c085bebc0a50fa816f36dc828cb2b5f
Mikleiper <mimuba55@gmail.com>
2024-11-12 16:59:03 +0100

    fet 3

---
+++ Exercici 3 - Nota mòdul.py
@@ -0,0 +1,9 @@
+nota = 8.3
+if nota < 5:
+    print(f"L'alumne ha suspès amb una nota de {nota}")
+elif nota <= 6.5:
+    print(f"L'alumne ha aprovat amb una nota de {nota}")
+elif nota <= 8:
+    print(f"L'alumne ha obtingut un notable amb una nota de {nota}")
+else:
+    print(f"L'alumne ha obtingut un excelent amb una nota de {nota}")
