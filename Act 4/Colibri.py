ebf88bec90df8eacaa8b54284c205a6a4bb7601c
Mikleiper <mimuba55@gmail.com>
2024-11-28 12:31:37 +0100

    sds

---
+++ Act 4/Colibri.py
@@ -0,0 +1,13 @@
+class Colibri
+#Constructor
+def__init__(self, color, velocitatAleteig, alimentació, longitud, pic):
+    #Atributs
+    self.color = color
+    self.velocitatAleteig = velocitatAleteig
+    self.alimentació = alimentació
+    self.longitud = longitud
+    self.pic = pic
+
+#Getter i Setters
+def getColor(self): Blau
+    return self.color
---
+++ Act 4/main.py
@@ -0,0 +1,3 @@
+from <main> import <Colibri>
+#Instancia objecte  Colibri
+colibri_1 = Colibri(color:"Blau", )
